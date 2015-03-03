7319b01eeb2177414b2ba4292070151b1efd7c28
Jonathan Hollinger <jmholl5@gmail.com>
2015-03-03 17:33:18 -0500

    Update Styling

--- js/statuslex.js
+++ js/statuslex.js
@@ -1,14 +1,16 @@
-/* Modules */
+/*--------------Modules--------------*/
 
 
 /* Main StatusLex App Module */
-var statuslex = angular.module('statuslex', [  'ngRoute', 'slControllers']);
+var statuslex = angular.module('statuslex', [  'ngRoute', 'slControllers', 'slServices']);
 
 /* Controllers Module */
 var slControllers = angular.module('slControllers', []);
 
+/* Services Module */
+var slServices = angular.module('slServices', []);
 
-/* Routing */
+/*--------------Routing--------------*/
 
 
 statuslex.config(['$routeProvider',
@@ -42,34 +44,18 @@ statuslex.config(['$routeProvider',
   }]);
 
 
-/* Controllers */
-
-
-/* Bulding Permit Record */
-slControllers.controller('PermitDetailCtrl', ['$scope', '$http', '$routeParams',
-  function ($scope, $http, $routeParams) {
-  var DataURL = 'http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT * FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" WHERE "_id"  =' + $routeParams.param
-    $http.get(DataURL).success(function(data) {
-    $scope.DetailData = data.result.records;
-    });
-}]);
+/*--------------Controllers--------------*/
 
 /* Bulding Permit Search */
 slControllers.controller('PermitSearchCtrl', ['$scope', '$http',
   function ($scope, $http) {
-  var order = '"Date" DESC'
-  var DataURL = 'http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT * FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" ORDER BY ' + order + ' LIMIT 25'
+  var DataURL = 'http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT * FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" ORDER BY "Date" DESC LIMIT 50'
+  var CountURL = 'http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT COUNT(*) FROM "2691aff1-e555-48d3-9188-aebf1fa8323e"'
     $http.get(DataURL).success(function(data) {
-      $scope.SearchData = data.result.records;
+    $scope.SearchData = data.result.records;
     });
-}]);
-
-/* Code Case Record */
-slControllers.controller('CodeDetailCtrl', ['$scope', '$http','$routeParams',
-  function ($scope, $http, $routeParams) {
-  var DataURL = 'http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT * FROM "ad346da7-ce88-4c77-a0e1-10ff09bb0622" WHERE "_id"  =' + $routeParams.param
-    $http.get(DataURL).success(function(data) {
-      $scope.DetailData = data.result.records;
+    $http.get(CountURL).success(function(data) {
+    $scope.RecordCount = data.result.records[0].count;
     });
 }]);
 
@@ -78,18 +64,42 @@ slControllers.controller('CodeSearchCtrl', ['$scope', '$http','$routeParams',
   function ($scope, $http, $routeParams) {
   var DataURL = 'http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT * FROM "ad346da7-ce88-4c77-a0e1-10ff09bb0622" LIMIT 25'
     $http.get(DataURL).success(function(data) {
-      $scope.SearchData = data.result.records;
+    $scope.SearchData = data.result.records;
     });
 }]);
 
+/* Bulding Permit Record */
+slControllers.controller('PermitDetailCtrl', ['$scope', '$http', '$routeParams',
+  function ($scope, $http, $routeParams) {
+  var DataURL = 'http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT * FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" WHERE "_id"  =' + $routeParams.param
+    $http.get(DataURL).success(function(data) {
+    $scope.DetailData = data.result.records;
+    });
+    $scope.RecordType = 'Building Permit'
+    $scope.Meta = 'Permits are issued by the Division of Building Inspection and the Division of Planning for a variety of activities including construction and certification of compliance with zoning. The permit information above is submitted by the applicant.'
+    $scope.Contact = 'If you have questions or concerns about permits, please contact the the Division of Building Inspection at (859) 425-2255.'
+}]);
 
-/* Filters */
+/* Code Case Record */
+slControllers.controller('CodeDetailCtrl', ['$scope', '$http','$routeParams',
+  function ($scope, $http, $routeParams) {
+  var DataURL = 'http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT * FROM "ad346da7-ce88-4c77-a0e1-10ff09bb0622" WHERE "_id"  =' + $routeParams.param
+    $http.get(DataURL).success(function(data) {
+    $scope.DetailData = data.result.records;
+    });
+    $scope.RecordType = 'Code Enforcement Case'
+    $scope.Meta = 'Code enforcement cases are opened based on citizen complaints for violations of nuisance code, the International Property Maintenance Code, and sidewalk regulations.'
+    $scope.Contact = 'If you have questions or concerns about code enforcement cases, please contact the the Division of Code Enforcement at (859) 425-2255.'
+}]);
+
+
+/*--------------Filters--------------*/
 
 
 /* titlecase filter */
 statuslex.filter('titlecase', function () {
   return function (input) {
-    var bigwords = /\b(aka|llc|hvac|[a-z]\/[a-z]|i|ii|iii|iv|v|vi|vii|viii|ix)\b/i;
+    var bigwords = /\b(ac|aka|llc|hvac|[a-z]\/[a-z]|i|ii|iii|iv|v|vi|vii|viii|ix)\b/i;
 	var smallwords = /\b(an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|to|vs)\b/i;
     var words = input.toLowerCase().split(' ');
     for (var i = 0; i < words.length; i++) {
@@ -100,3 +110,5 @@ statuslex.filter('titlecase', function () {
     return words.join(' ');
   }
 });
+
+/*--------------Services--------------*/
